202cf8e9e8d2196a2c51d92ad9791cb9101c3846
Dana Speianu <speianu.dana@gmail.com>
2022-02-14 20:14:53 +0200

    add project

---
+++ scripts/Camera.gd
@@ -0,0 +1,51 @@
+extends Camera
+ 
+export(NodePath) onready var target  = get_node(target)
+export(Resource) var setup
+var _pressed := false
+var _last_pos
+
+func _ready():
+	if setup.target_offset == Vector3.ZERO:
+		setup.target_offset = self.transform.origin - target.transform.origin - setup.anchor_offset
+	if setup.look_target == Vector3.ZERO:
+		setup.look_target  = Vector3(0, 0, 100.0)
+	setup.pitch_limit.x = deg2rad(setup.pitch_limit.x)
+	setup.pitch_limit.y = deg2rad(setup.pitch_limit.y)
+	
+func _process(delta):
+	self.transform.origin = target.transform.origin + setup.anchor_offset
+	var target_offset = setup.target_offset
+	var look_at = setup.look_target
+		
+	var up_down_axis = Vector3.RIGHT.rotated(Vector3.UP, setup.rotation.y)
+	target_offset = target_offset.rotated(Vector3.UP, setup.rotation.y)
+	look_at = look_at.rotated(Vector3.UP, setup.rotation.y)
+	target_offset = target_offset.rotated(up_down_axis,setup.rotation.x)
+	look_at = look_at.rotated(up_down_axis,setup.rotation.x)
+		
+	self.transform.origin += target_offset
+	self.look_at(look_at, Vector3.UP)
+	collide()
+func _unhandled_input(event):
+	if event is InputEventScreenTouch and event.pressed == true:
+		if _pressed == false:
+			_pressed = true
+			_last_pos = event.position
+	if event is InputEventScreenTouch and event.pressed == false:
+		_pressed = false
+	if event is InputEventScreenDrag and _pressed == true:
+		var diff : Vector2 = _last_pos - event.position
+		diff = diff / 100.0
+		setup.rotation.y+=diff.x
+		setup.rotation.x-=diff.y
+		setup.rotation.x = clamp(setup.rotation.x, setup.pitch_limit.x, setup.pitch_limit.y)
+		_last_pos = event.position
+
+func collide():
+	var start = target.transform.origin + setup.anchor_offset
+	var end = self.transform.origin
+	var space_state = get_world().direct_space_state
+	var col = space_state.intersect_ray(start, end)
+	if not col.empty():
+		self.transform.origin = col.position
---
+++ scripts/Player.gd
@@ -0,0 +1,51 @@
+extends KinematicBody
+
+export(NodePath) var animationtree
+export(NodePath) onready var camera = get_node(camera)
+
+onready var _anim_tree = get_node(animationtree)
+var gravity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var root_motion : Transform = _anim_tree.get_root_motion_transform()
+	
+	var v = root_motion.origin / delta
+	if is_on_floor():
+		gravity = Vector3.ZERO
+	else:
+		gravity = Vector3(0.0, -9.83, 0.0)
+	v+=gravity
+	
+	var dir : Vector3 = Vector3.ZERO
+	
+	if Input.is_action_pressed("forward"):
+		dir.z += 1.0
+	if Input.is_action_pressed("back"):
+		dir.z -= 1.0
+	if Input.is_action_pressed("left"):
+		dir.z += 1.0
+	if Input.is_action_pressed("right"):
+		dir.x += 1.0
+	if Input.is_action_pressed("forward"):
+		dir.x -= 1.0
+		
+	if dir.length_squared() > 0.01:
+		dir = dir.rotated(Vector3.UP, camera.setup.rotation.y)
+	
+		var player_heading_2d := Vector2(self.transform.basis.z.x, self.transform.basis.z.z)
+		var desired_heading_2d := Vector2(dir.x, dir.z)
+		var phi : float = desired_heading_2d.angle_to(player_heading_2d)
+		phi = phi + delta + 3.0
+		self.rotation.y += phi
+		v = v.rotated(Vector3.UP, self.rotation.y)
+		if Input.is_action_pressed("sprint"):
+			_anim_tree["parameters/playback"].travel("Running")
+		else:
+			_anim_tree["parameters/playback"].travel("Walking")
+	else:
+		_anim_tree["parameters/playback"].travel("Idle")
+		v = v.rotated(Vector3.UP, self.rotation.y)
+		
+	_anim_tree["parameters/conditions/jump"] = Input.is_action_pressed("jump")
+	move_and_slide(v, Vector3.UP)
